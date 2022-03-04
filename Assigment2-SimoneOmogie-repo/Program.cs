99035876e32f502831d53db5e2731bfd2628b240
omogi <omogi@DESKTOP-9SE1OVO>
2022-03-04 04:38:05 -0700

    Add project files.

---
+++ Assigment2-SimoneOmogie-repo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assigment2_SimoneOmogie_repo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
